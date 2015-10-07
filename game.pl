3de16b917580a4fd908e5c3aad6034361521b838
Djowood <jolan.cornevin@etu.univ-lyon1.fr>
2015-10-07 12:01:36 +0200

    Ajout du gameover

--- game.pl
+++ game.pl
@@ -1,13 +1,25 @@
 :-dynamic avion1/3, avion2/3.
 
 avion1(1,1,10).
-avion2(1,1,10).
+avion2(1,17,10).
 
-reset():-
-	retract(avion1(_,_,0));
-	retract(avion2(_,_,0)).
+reset():-retract(avion1(_,_,0)).
+reset():-retract(avion2(_,_,0)).
 
-gameover():- avion1(_,_,0), avion2(_,_,0), print('égalité').
+%TODO
+gameover():-avion1(X, _, _), X <0, print('Joueur 2 a gagné').
+gameover():-avion1(X, _, _), X >16, print('Joueur 2 a gagné').
+gameover():-avion1(-, Y, _), Y <0, print('Joueur 2 a gagné').
+gameover():-avion1(-, Y, _), Y >16, print('Joueur 2 a gagné').
+
+gameover():-avion2(X, _, _), X <0, print('Joueur 1 a gagné').
+gameover():-avion2(X, _, _), X >16, print('Joueur 1 a gagné').
+gameover():-avion2(-, Y, _), Y <0, print('Joueur 1 a gagné').
+gameover():-avion2(-, Y, _), Y >16, print('Joueur 1 a gagné').
+
+
+gameover():-avion1(X, Y, _), avion2(X, Y, _), print('égalité par colision').
+gameover():- avion1(_,_,0), avion2(_,_,0), print('égalité par mort des deux avions').
 gameover():- avion1(_,_,0), print('Joueur 2 a gagné').
 gameover():- avion2(_,_,0), print('Joueur 1 a gagné').
 
@@ -15,22 +27,23 @@ update():-updateAvion1(), updateAvion2().
 
 updateAvion1():-
         %récupère X (pt de vie)
-	avion1(1,1,X),
+	avion1(Y,Z,X),
 	%on le retire de la base
-	retract(avion1(1,1,X)),
-	Y is X-1,
-	assert(avion1(1,1,Y)).
+	retract(avion1(Y,Z,X)),
+	X1 is X-1,
+	assert(avion1(Y,Z,X1)).
 
 updateAvion2():-
 	%récupère X (pt de vie)
-	avion2(1,1,X),
+	avion2(Y,Z,X),
 	%on le retire de la base
-	retract(avion2(1,1,X)),
-	Y is X-1,
-	assert(avion2(1,1,Y)).
+	retract(avion2(Y,Z,X)),
+	X1 is X-1,
+	assert(avion2(Y,Z,X1)).
 
 etape():-
 	update(),
+	%display()
 	game().
 
 game():- gameover(), !.
