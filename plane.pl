51ebb8c1c812e7645d0c494b5c94bd87a9ababda
unknown <dwobrock@IF501-202-04.insa-lyon.fr>
2015-10-08 17:49:05 +0200

    Plane actions done, in english and all working. Executing the three actions of each at the same time works too.

--- plane.pl
+++ plane.pl
@@ -3,20 +3,27 @@
 
 % Faits qui definissent les positions initiales des avions.
 % Le premier argument correspond à l'indice de l'avion
-plane(1, 0, 0, 3, b).
-plane(2, 15, 15, 3, h).
-
-
-% On passe en paramètre l'indice I de l'avion et un coup à jouer.
-% Ce prédicat 
-updatePlane(Index, Coup) :- plane(Index,X,Y,A,B), 
-					  retract(plane(Index,X,Y,A,B)),
-					  Z is X+1,
-					  W is Y+1,
-					  assert(plane(Index,Z,W,A,B)).
+plane(1, 0, 0, 3, 'S').
+plane(2, 15, 15, 3, 'N').
 
 % Execute les coups de même indice en même temps
 updatePlanes([], []).
-updatePlanes([Coup1|ListCoup1], [Coup2|ListCoup2]) :- 	updatePlane(1, Coup1),
-														updatePlane(2, Coup2),
-														updatePlanes(ListCoup1, ListCoup2).
+updatePlanes([Action1|ActionList1], [Action2|ActionList2]) :- 	callPlaneAction(1, Action1),
+																callPlaneAction(2, Action2),
+																% TODO : check if a plane can fire on the other
+																updatePlanes(ActionList1, ActionList2).
+
+% Big kind of switch, choosing between all implemented actions 
+callPlaneAction(Idx, Action) :- Action == 'F', actionForward(Idx), !.
+callPlaneAction(Idx, Action) :- Action == 'FF', actionFastForward(Idx), !.
+callPlaneAction(Idx, Action) :- Action == 'RT', actionRightTurn(Idx), !.
+callPlaneAction(Idx, Action) :- Action == 'LT', actionLeftTurn(Idx), !.
+callPlaneAction(Idx, Action) :- Action == 'UT', actionUTurn(Idx), !.
+% Error handling if unknown action. TODO : better error throwing than "1 == 2"...
+callPlaneAction(Idx, Action) :- print('Unknown action '), print(Action), print(' for index '), print(Idx), 1 == 2.
+
+
+%updatePlanes(['FF', 'LT', 'UT'], ['LT', 'RT', 'F']).
+%plane(Idx,X,Y,Life,Orientation).
+%callPlaneAction(1, 'R').
+%callPlaneAction(1, 'F').
--- plane_actions.pl
+++ plane_actions.pl
@@ -1,130 +1,111 @@
+% FORWARD
+actionForward(Idx) :- 	plane(Idx, X, Y, Life, Orientation),
+						Orientation == 'N',
+						retract(plane(Idx, X, Y, Life, Orientation)),
+						NewY is Y-1,
+						assert(plane(Idx, X, NewY, Life, Orientation)).
 
-AvanceRapide(i) :- avion(i,X,Y,vie,orientation).orientation \== d
-					retract(avion),
-					newX is X+2,
-					assert(avion(i,newX,Y,vie,orientation).
+actionForward(Idx) :- 	plane(Idx, X, Y, Life, Orientation),
+						Orientation == 'S',
+						retract(plane(Idx, X, Y, Life, Orientation)),
+						NewY is Y+1,
+						assert(plane(Idx, X, NewY, Life, Orientation)).
+						
+actionForward(Idx) :- 	plane(Idx, X, Y, Life, Orientation),
+						Orientation == 'E',
+						retract(plane(Idx, X, Y, Life, Orientation)),
+						NewX is X+1,
+						assert(plane(Idx, NewX, Y, Life, Orientation)).
 
-AvanceRapide(i) :- avion(i,X,Y,vie,orientation).orientation \== g
-					retract(avion),
-					newX is X-2,
-					assert(avion(i,newX,Y,vie,orientation).
-					
-AvanceRapide(i) :- avion(i,X,Y,vie,orientation).orientation \== h
-					retract(avion),
-					newY is Y+2,
-					assert(avion(i,X,newY,vie,orientation).
-					
-AvanceRapide(i) :- avion(i,X,Y,vie,orientation).orientation \== b
-					retract(avion),
-					Y is X-2,
-					assert(avion(i,X,newY,vie,orientation).
-					
-					
-					
-					
-Avance(i) :- avion(i,X,Y,vie,orientation).orientation \== d
-					retract(avion),
-					newX is X+1,
-					assert(avion(i,newX,Y,vie,orientation).
+actionForward(Idx) :- 	plane(Idx, X, Y, Life, Orientation),
+						Orientation == 'W',
+						retract(plane(Idx, X, Y, Life, Orientation)),
+						NewX is X-1,
+						assert(plane(Idx, NewX, Y, Life, Orientation)).
 
-Avance(i) :- avion(i,X,Y,vie,orientation).orientation \== g
-					retract(avion),
-					newX is X-1,
-					assert(avion(i,newX,Y,vie,orientation).
-					
-Avance(i) :- avion(i,X,Y,vie,orientation).orientation \== h
-					retract(avion),
-					Y is Y+1,
-					assert(avion(i,X,newY,vie,orientation).
-					
-Avance(i) :- avion(i,X,Y,vie,orientation).orientation \== b
-					retract(avion),
-					Y is X-1,
-					assert(avion(i,X,newY,vie,orientation).
-					
-					
-					
+% FAST FORWARD						
+actionFastForward(Idx) :- 	actionForward(Idx),
+							actionForward(Idx).
 
-VirageDroit(i) :- avion(i,X,Y,vie,orientation).orientation \== d
-					retract(avion),
-					newX is X+1,
-					newY is Y-1,
-					newOrientation=b,
-					assert(avion(i,newX,newY,vie,newOrientation).
+% TURN RIGHT
+actionRightTurn(Idx) :- actionForward(Idx),
+						turnRight(Idx),
+						actionForward(Idx).
 
-VirageDroit(i) :- avion(i,X,Y,vie,orientation).orientation \== g
-					retract(avion),
-					newX is X-1,
-					newY is Y+1,
-					newOrientation=h,
-					assert(avion(i,newX,newY,vie,newOrientation).
-					
-VirageDroit(i) :- avion(i,X,Y,vie,orientation).orientation \== h
-					retract(avion),
-					newX is X+1,
-					newY is Y+1,
-					newOrientation=d,
-					assert(avion(i,newX,newY,vie,newOrientation).
-					
-VirageDroit(i) :- avion(i,X,Y,vie,orientation).orientation \== b
-					retract(avion),
-					newX is X-1,
-					newY is Y-1,
-					newOrientation=g,
-					assert(avion(i,newX,newY,vie,newOrientation).
-					
-					
-					
-					
-VirageGauche(i) :- avion(i,X,Y,vie,orientation).orientation \== d
-					retract(avion),
-					newX is X+1,
-					newY is Y+1,
-					newOrientation=h,
-					assert(avion(i,newX,newY,vie,newOrientation).
+turnRight(Idx) :- 	plane(Idx, X, Y, Life, Orientation),
+					Orientation == 'N',
+					retract(plane(Idx, X, Y, Life, Orientation)),
+					NewOrientation = 'E',
+					assert(plane(Idx, X, Y, Life, NewOrientation)).
+				
+turnRight(Idx) :- 	plane(Idx, X, Y, Life, Orientation),
+					Orientation == 'E',
+					retract(plane(Idx, X, Y, Life, Orientation)),
+					NewOrientation = 'S',
+					assert(plane(Idx, X, Y, Life, NewOrientation)).
 
-VirageGauche(i) :- avion(i,X,Y,vie,orientation).orientation \== g
-					retract(avion),
-					newX is X-1,
-					newY is Y-1,
-					newOrientation=b,
-					assert(avion(i,newX,newY,vie,newOrientation).
-					
-VirageGauche(i) :- avion(i,X,Y,vie,orientation).orientation \== h
-					retract(avion),
-					newX is X-1,
-					newY is Y+1,
-					newOrientation=g,
-					assert(avion(i,newX,newY,vie,newOrientation).
-					
-VirageGauche(i) :- avion(i,X,Y,vie,orientation).orientation \== b
-					retract(avion),
-					newX is X+1,
-					newY is Y-1,
-					newOrientation=g,
-					assert(avion(i,newX,newY,vie,newOrientation).
-					
-					
-					
+turnRight(Idx) :- 	plane(Idx, X, Y, Life, Orientation),
+					Orientation == 'S',
+					retract(plane(Idx, X, Y, Life, Orientation)),
+					NewOrientation = 'W',
+					assert(plane(Idx, X, Y, Life, NewOrientation)).
 
-DemiTour(i) :- avion(i,X,Y,vie,orientation).orientation \== d
-					retract(avion),
-					newOrientation=g,
-					assert(avion(i,X,Y,vie,newOrientation).
+turnRight(Idx) :- 	plane(Idx, X, Y, Life, Orientation),
+					Orientation == 'W',
+					retract(plane(Idx, X, Y, Life, Orientation)),
+					NewOrientation = 'N',
+					assert(plane(Idx, X, Y, Life, NewOrientation)).				
 
-DemiTour(i) :- avion(i,X,Y,vie,orientation).orientation \== g
-					retract(avion),
-					newOrientation=d,
-					assert(avion(i,X,Y,vie,newOrientation).
-					
-DemiTour(i) :- avion(i,X,Y,vie,orientation).orientation \== h
-					retract(avion),
-					newOrientation=b,
-					assert(avion(i,X,Y,vie,newOrientation).
-					
-DemiTour(i) :- avion(i,X,Y,vie,orientation).orientation \== b
-					retract(avion),
-					newOrientation=h,
-					assert(avion(i,X,Y,vie,newOrientation).						
+% TURN LEFT				
+actionLeftTurn(Idx) :- 	actionForward(Idx),
+						turnLeft(Idx),
+						actionForward(Idx).						
 
+turnLeft(Idx) :- 	plane(Idx, X, Y, Life, Orientation),
+					Orientation == 'N',
+					retract(plane(Idx, X, Y, Life, Orientation)),
+					NewOrientation = 'W',
+					assert(plane(Idx, X, Y, Life, NewOrientation)).	
+
+turnLeft(Idx) :- 	plane(Idx, X, Y, Life, Orientation),
+					Orientation == 'W',
+					retract(plane(Idx, X, Y, Life, Orientation)),
+					NewOrientation = 'S',
+					assert(plane(Idx, X, Y, Life, NewOrientation)).	
 					
+turnLeft(Idx) :- 	plane(Idx, X, Y, Life, Orientation),
+					Orientation == 'S',
+					retract(plane(Idx, X, Y, Life, Orientation)),
+					NewOrientation = 'E',
+					assert(plane(Idx, X, Y, Life, NewOrientation)).	
+					
+turnLeft(Idx) :- 	plane(Idx, X, Y, Life, Orientation),
+					Orientation == 'E',
+					retract(plane(Idx, X, Y, Life, Orientation)),
+					NewOrientation = 'N',
+					assert(plane(Idx, X, Y, Life, NewOrientation)).	
+
+% U-TURN					
+actionUTurn(Idx) :- plane(Idx, X, Y, Life, Orientation),
+					Orientation == 'N',
+					retract(plane(Idx, X, Y, Life, Orientation)),
+					NewOrientation = 'S',
+					assert(plane(Idx, X, Y, Life, NewOrientation)).
+					
+actionUTurn(Idx) :- plane(Idx, X, Y, Life, Orientation),
+					Orientation == 'S',
+					retract(plane(Idx, X, Y, Life, Orientation)),
+					NewOrientation = 'N',
+					assert(plane(Idx, X, Y, Life, NewOrientation)).
+					
+actionUTurn(Idx) :- plane(Idx, X, Y, Life, Orientation),
+					Orientation == 'E',
+					retract(plane(Idx, X, Y, Life, Orientation)),
+					NewOrientation = 'W',
+					assert(plane(Idx, X, Y, Life, NewOrientation)).
+					
+actionUTurn(Idx) :- plane(Idx, X, Y, Life, Orientation),
+					Orientation == 'W',
+					retract(plane(Idx, X, Y, Life, Orientation)),
+					NewOrientation = 'E',
+					assert(plane(Idx, X, Y, Life, NewOrientation)).
