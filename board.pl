1f670596bba6c9473d6eba663bbd8923a9e9c5e9
unknown <dwobrock@IF501-202-04.insa-lyon.fr>
2015-10-08 18:25:19 +0200

    first working version of the game :D, but play with commands

--- board.pl
+++ board.pl
@@ -1,8 +1,6 @@
 :- dynamic board/1.
 :- [display].
 
-plane(1,0, 0, 3, b).
-plane(2,15, 15, 3, h).
 %% le terrain de jeu 16 *16
 defaultBoard([ [v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v],
         [v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v],
@@ -27,10 +25,10 @@ defaultBoard([ [v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v],
 replace([_|T], 0, X, [X|T]).
 replace([H|T], I, X, [H|R]):- I > 0, I1 is I-1, replace(T, I1, X, R).
 
-replaceBoard(X, Y, V, B, R):-
-			nth0(X, B, E),
-			replace(E, Y, V, M),
-			replace(B, X, M, R).
+replaceBoard(X, Y, Value, Board, Result):-
+			nth0(X, Board, E),
+			replace(E, X, Value, M),
+			replace(Board, Y, M, Result).
 displayBoard :- 
 			defaultBoard(B),
 			plane(1, C1x, C1y,_,O1),
--- game.pl
+++ game.pl
@@ -1,53 +1,33 @@
-:-dynamic avion1/3, avion2/3.
+:- [plane].
+:- [board].
 
-avion1(1,1,10).
-avion2(1,17,10).
+reset:-retract(avion1(_,_,0)).
+reset:-retract(avion2(_,_,0)).
 
-reset():-retract(avion1(_,_,0)).
-reset():-retract(avion2(_,_,0)).
+%TODO : change avec plane(1,...) etc
+gameover:-avion1(X, _, _), X <0, print('Joueur 2 a gagné').
+gameover:-avion1(X, _, _), X >16, print('Joueur 2 a gagné').
+gameover:-avion1(-, Y, _), Y <0, print('Joueur 2 a gagné').
+gameover:-avion1(-, Y, _), Y >16, print('Joueur 2 a gagné').
 
-%TODO
-gameover():-avion1(X, _, _), X <0, print('Joueur 2 a gagné').
-gameover():-avion1(X, _, _), X >16, print('Joueur 2 a gagné').
-gameover():-avion1(-, Y, _), Y <0, print('Joueur 2 a gagné').
-gameover():-avion1(-, Y, _), Y >16, print('Joueur 2 a gagné').
-
-gameover():-avion2(X, _, _), X <0, print('Joueur 1 a gagné').
-gameover():-avion2(X, _, _), X >16, print('Joueur 1 a gagné').
-gameover():-avion2(-, Y, _), Y <0, print('Joueur 1 a gagné').
-gameover():-avion2(-, Y, _), Y >16, print('Joueur 1 a gagné').
+gameover:-avion2(X, _, _), X <0, print('Joueur 1 a gagné').
+gameover:-avion2(X, _, _), X >16, print('Joueur 1 a gagné').
+gameover:-avion2(-, Y, _), Y <0, print('Joueur 1 a gagné').
+gameover:-avion2(-, Y, _), Y >16, print('Joueur 1 a gagné').
 
 
-gameover():-avion1(X, Y, _), avion2(X, Y, _), print('égalité par colision').
-gameover():- avion1(_,_,0), avion2(_,_,0), print('égalité par mort des deux avions').
-gameover():- avion1(_,_,0), print('Joueur 2 a gagné').
-gameover():- avion2(_,_,0), print('Joueur 1 a gagné').
+gameover:-avion1(X, Y, _), avion2(X, Y, _), print('égalité par colision').
+gameover:- avion1(_,_,0), avion2(_,_,0), print('égalité par mort des deux avions').
+gameover:- avion1(_,_,0), print('Joueur 2 a gagné').
+gameover:- avion2(_,_,0), print('Joueur 1 a gagné').
 
-update():-updateAvion1(), updateAvion2().
+etape:-
+	updatePlanes(['FF', 'LT', 'UT'], ['LT', 'RT', 'F']), % test values, a supprimer
+	displayBoard.
+	%,game(). % a décommenter pour boucle de jeu
 
-updateAvion1():-
-        %récupère X (pt de vie)
-	avion1(Y,Z,X),
-	%on le retire de la base
-	retract(avion1(Y,Z,X)),
-	X1 is X-1,
-	assert(avion1(Y,Z,X1)).
+game:- gameover, !.
+game:- etape.
 
-updateAvion2():-
-	%récupère X (pt de vie)
-	avion2(Y,Z,X),
-	%on le retire de la base
-	retract(avion2(Y,Z,X)),
-	X1 is X-1,
-	assert(avion2(Y,Z,X1)).
-
-etape():-
-	updatePlanes(),
-	displayBoard()
-	game().
-
-game():- gameover(), !.
-game():- etape().
-
-lancer():-reset(); game().
+lancer:-reset; game.
 %['C:/Users/Djowood/Documents/INSA/Prolog/Projet/As_des_As/game'].
