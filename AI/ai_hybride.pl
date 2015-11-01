af8b52d78f1db16ed401e7443933a8606a79dbd6
Mohamed Haidara <mohamed.haidara@insa-lyon.fr>
2015-11-01 03:39:55 +0100

    Amélioration de l'heuristique pour prendre en compte les pertes reçues...

--- AI/ai_hybride.pl
+++ AI/ai_hybride.pl
@@ -9,18 +9,20 @@ gain(0).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-% Implémentation d'une intelligence articificielle hybride dans le sens où elle essaie d'attaquer sans se faire tirer dessus aussi.
-% Son but est de maximiser ses tirs tout en minimisant les possibilités de tir de l'adversaire.
-% => Algo min/max
-% Principe : A partir de la position actuelle du joueur, on construit l'arbre de jeu c'est à dire tous les coups 
-% (triplets d'actions) possibles tout en ne sortant pas du tableau. On évalue les coups au niveau des feuilles
-% de cet arbre en utilisant notre fonction heuristique et en faisant remonter le max ou le min.
+% Implémentation d'une intelligence articificielle hybride dans le sens où elle essaie de tirer sans se faire tirer dessus aussi.
+% Son but est de maximiser ses tirs tout en minimisant les possibilités de tirs de l'adversaire.
+% Elle suppose que l'adversaire joue les coups qui sont le plus en sa faveur.
+% Principe : A partir d'un état du jeu, on construit l'arbre de jeu c'est à dire tous les coups (triplets d'actions) possibles 
+% pour l'IA et l'autre joueur tout en ne sortant pas du tableau. On évalue ensuite les coups grâce à une heuristique pour faire remonter
+% les max.
+% L'arbre de recherche n'a pas une grande profondeur mais un nombre très important de noeuds dû aux coups possibles à chaque "step".
+% TODO : élagage alpha beta  
 
-% Idx : identifiant de l'avion de l'IA en entrée
-playHybride(Idx,Gain) :-	otherPlayer(Idx, OtherIdx), 
+% Pas encore fini
+playHybride(Idx,Sol) :-	otherPlayer(Idx, OtherIdx), 
 					    generate(Idx, CoupIA),
-					    %generate(OtherIdx,CoupOther),
-					    h(Idx,OtherIdx,CoupIA,Gain).					      
+					    generate(OtherIdx,CoupOther),
+					    h(Idx,OtherIdx,CoupIA,CoupOther,Sol).					      
 	
 
 % Heuristique qui calcule le gain associé à un coup.
@@ -28,23 +30,33 @@ playHybride(Idx,Gain) :-	otherPlayer(Idx, OtherIdx),
 % OtherPlayer : indice de l'autre joueur en entrée
 % CoupIA : Liste d'actions de l'IA en entrée
 % CoupOther : Liste d'actions de l'autre joueur
-% Gain : gain associé en sortie
-% Au départ on met le gain à zero pour le moment et on choisit l'avion 3 et comme avions temporaires
+% Gain : gain associé en sortie.
+% Le gain correspond à une valeur entre -3 et 3 : 
+%  - une valeur négative correspond à un nombre de tirs encaissés > au nombre de tirs donnés
+%  - 0 implique pas de tirs ou 3 tirs donnés et 3 tirs reçus.
+%  - Une valeur positive correspond à un nombre de tirs donnés > nombre de tirs encaissés.
+% L'objectif sera ainsi de maximiser ce gain.
+% Au départ on met le gain à zero et on choisit l'avion 3 et 4 comme avions temporaires
 h(Idx,OtherPlayer,CoupIA,CoupOther,Gain) :- hr(Idx,OtherPlayer,3,4,CoupIA,CoupOther,0,Gain).						   			
 
-% 
+% Effectue l'appel récursif pour le calcul de l'heuristique
 % TmpPlane1 et TmpPlane2 : avions temporaires en entrée
-% Action : action courante en entrée
+% ActionIA : action courante de l'IA en entrée
+% ActionOther : action courante de l'autre joueur en entrée
 % Acc : sert d'accumulateur pour le calcul du gain
+% Note : le nombre d'appel récursifs est lié au nombre d'actions dans la liste. Pour le moment on en a que 3.
+% Avec cette méthode, on pourra étendre ce prédicat si on ne connait pas le nombre de coups à l'avance.
 hr(_,_,_,_,[],[],Gain,Gain).
-hr(Idx,OtherPlayer,TmpPlane1,TmpPlane2,[ActionIA|CoupIA],[ActionOther|CoupOther],Acc,Gain):- update(Idx, TmpPlane1),
-							   			update(OtherPlayer, TmpPlane2),
-							   			callPlaneAction(TmpPlane1, ActionIA),
-							   			callPlaneAction(TmpPlane2, ActionOther),
-							   			(canFire(TmpPlane1, TmpPlane2) -> NewGain is Acc + 1; NewGain is Acc),
-							   			NewTmpPlane1 is TmpPlane1 + 2,
-							   			NewTmpPlane2 is TmpPlane2 + 2,
-							   			hr(TmpPlane1,TmpPlane2,NewTmpPlane1,NewTmpPlane2,CoupIA,CoupOther,NewGain,Gain).			   	
+hr(Idx,OtherPlayer,TmpPlane1,TmpPlane2,[ActionIA|CoupIA],[ActionOther|CoupOther],AccGain,Gain):- 
+										update(Idx, TmpPlane1), % Remplissage du premier avion temporaire
+							   			update(OtherPlayer, TmpPlane2), % Remplissage du premier avion temporaire
+							   			callPlaneAction(TmpPlane1, ActionIA), %  Déplacement du premier avion
+							   			callPlaneAction(TmpPlane2, ActionOther), %  Déplacement du second avion
+							   			(canFire(TmpPlane1, TmpPlane2) -> NewGain is AccGain + 1; % +1 si l'ia peut tirer
+							   						( canFire(TmpPlane2, TmpPlane1) -> NewGain is AccGain - 1;NewGain is AccGain )), % -1 si l'autre joueur peut tirer
+							   			NewTmpPlane1 is TmpPlane1 + 2, % Passage au prochain avion temporaire
+							   			NewTmpPlane2 is TmpPlane2 + 2, % Passage au prochain avion temporaire
+							   			hr(TmpPlane1,TmpPlane2,NewTmpPlane1,NewTmpPlane2,CoupIA,CoupOther,NewGain,Gain). % Appel récursif			   	
  
 % Met tous les triplets dans une liste (j'en aurai peut etre pas besoin)
 generateAll(Idx,Liste) :- setof([Action1,Action2,Action3],generate(Idx,[Action1,Action2,Action3]),Liste).
