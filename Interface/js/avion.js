bed7e5bc2650d3ff43204e0838875763eed3e473
robinroyer <robin.royer974@gmail.com>
2015-11-05 22:00:35 +0100

    changing ihm text to english

--- Interface/js/avion.js
+++ Interface/js/avion.js
@@ -25,7 +25,7 @@ Avion.prototype.debug_ihm = function() {
 
 
 	$("#debug_" + this.nom ).append(
-	 "<div class='col-xs-3' id='debug_" + this.nom+ "_nom' > Avion " + this.player+ ": </div>"
+	 "<div class='col-xs-3' id='debug_" + this.nom+ "_nom' > Plane " + this.player+ ": </div>"
 	+	"<div class='col-xs-2 ' id='debug_" + this.nom + "_x'> x : " + this.x + "</div>"
 	+ "<div class='col-xs-2 ' id='debug_" + this.nom + "_y'> y : " + this.y + "</div>"
 	+ "<div class='col-xs-5 ' id='debug_" + this.nom + "_orientation'> orientation : " + this.orientation + "</div>");
